352b9c67ff9062d24651c6a3f89e394b1bd7c58b
Kurt Radecki <keradecki99@gmail.com>
2025-05-06 13:19:39 +0000

    chore: removed errant comment

--- inputs.tf
+++ inputs.tf
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Future versions of the script may add modularity if it is needed, though the intent of the script is to be simple and have limited configurability.
-
 
 variable  project-id {}
 variable  region1 {}
