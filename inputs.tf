4088e1ab1c61c259cc076e89bf0e0c1385bf1702
Kurt Radecki <keradecki99@gmail.com>
2025-05-06 12:57:25 +0000

    chore: added variables for route config

--- inputs.tf
+++ inputs.tf
@@ -19,3 +19,15 @@ variable  project-id {}
 variable  region1 {}
 variable  vpc1name {}
 variable  vpc2name {}
+variable vpc1_cldrouter_config {
+  type = object({
+      all_subnets = bool
+      ip_ranges   = map(string)
+    })
+}
+variable vpc2_cldrouter_config {
+  type = object({
+      all_subnets = bool
+      ip_ranges   = map(string)
+    })
+}
